d41962638836fd7bee1fefc3b2e386e303a7cda1
Alhassan-Ali7 <132752068+Alhassan-Ali7@users.noreply.github.com>
2023-06-11 10:56:28 +0300

    add colors list view to edit note view

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff62fcd7);
 
 const kNotesBox = 'notes_box';
+
+List<Color> kColors = const [
+  Color(0xffda2c38),
+  Color(0xff226f54),
+  Color(0xff87c38f),
+  Color(0xfff4f0bb),
+  Color(0xff43291f),
+];
--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
@@ -34,13 +35,7 @@ class ColorsListView extends StatefulWidget {
 }
 
 class _ColorsListViewState extends State<ColorsListView> {
-  List<Color> colors = const [
-    Color(0xffda2c38),
-    Color(0xff226f54),
-    Color(0xff87c38f),
-    Color(0xfff4f0bb),
-    Color(0xff43291f),
-  ];
+
 
   int currentIndex = 0;
 
@@ -50,18 +45,18 @@ class _ColorsListViewState extends State<ColorsListView> {
       height: 38 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: colors.length,
+        itemCount: kColors.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 6.0),
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
-                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
+                BlocProvider.of<AddNoteCubit>(context).color = kColors[index];
                 setState(() {});
               },
               child: ColorItem(
-                color: colors[index],
+                color: kColors[index],
                 isActive: currentIndex == index,
               ),
             ),
